30ac813a123dbd22f8fc1752fa950159461ad680
John-John Tedro <udoprog@spotify.com>
2014-09-09 11:48:37 +0200

    Remove other variants of MetricGroup.

--- src/main/java/com/spotify/heroic/metrics/model/MetricGroup.java
+++ src/main/java/com/spotify/heroic/metrics/model/MetricGroup.java
@@ -8,7 +8,6 @@ import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.spotify.heroic.model.DataPoint;
-import com.spotify.heroic.model.Series;
 
 @Data
 public final class MetricGroup {
@@ -18,22 +17,7 @@ public final class MetricGroup {
     @JsonCreator
     public static MetricGroup create(
             @JsonProperty(value = "group", required = true) Map<String, String> group,
-            @JsonProperty(value = "series", required = true) Series series,
-            @JsonProperty(value = "timeSerie", required = true) Series timeSeries,
             @JsonProperty(value = "datapoints", required = true) List<DataPoint> datapoints) {
-        final Map<String, String> g;
-
-        if (group != null) {
-            g = group;
-        } else if (series != null) {
-            g = series.getTags();
-        } else if (timeSeries != null) {
-            g = timeSeries.getTags();
-        } else {
-            throw new IllegalArgumentException(
-                    "Neither series nor timeSeries was specified");
-        }
-
-        return new MetricGroup(g, datapoints);
+        return new MetricGroup(group, datapoints);
     }
 }
