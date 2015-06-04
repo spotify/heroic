1d92cff75002003bc46349a44844b8788fd751c3
Francesc Zacarias <francesc@spotify.com>
2015-06-04 18:11:52 +0200

    [licensing] add license to all code files + tool to check

---
+++ tools/add_license.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+TMPFILE=$(mktemp)
+
+cleanup() {
+    rm "$TMPFILE"
+}
+
+trap cleanup 0
+
+for FILE in $(find . -type f -name "*.java" | grep -v '/src/test/');do
+    # check if file has license.
+    # if not, prepend it
+    if ! tools/license_matcher.py "$FILE" >/dev/null;then
+        echo "Prepend license to $FILE"
+        cat tools/java.header "$FILE" > "$TMPFILE"
+        cat "$TMPFILE" > "$FILE"
+    fi
+
+done
