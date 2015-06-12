618e0f398113eb0e34af9badbe16bfec4e806029
John-John Tedro <udoprog@spotify.com>
2015-06-12 18:48:02 +0200

    [suggest/elasticsearch] move mappings into resources

--- suggest/elasticsearch/src/main/java/com/spotify/heroic/suggest/elasticsearch/ElasticsearchSuggestModule.java
+++ suggest/elasticsearch/src/main/java/com/spotify/heroic/suggest/elasticsearch/ElasticsearchSuggestModule.java
@@ -22,6 +22,7 @@
 package com.spotify.heroic.suggest.elasticsearch;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -30,8 +31,7 @@ import javax.inject.Singleton;
 
 import lombok.ToString;
 
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -42,7 +42,6 @@ import com.google.inject.PrivateModule;
 import com.google.inject.Provides;
 import com.spotify.heroic.concurrrency.ReadWriteThreadPools;
 import com.spotify.heroic.elasticsearch.Connection;
-import com.spotify.heroic.elasticsearch.ElasticsearchUtils;
 import com.spotify.heroic.elasticsearch.ManagedConnectionFactory;
 import com.spotify.heroic.statistics.LocalMetadataBackendReporter;
 import com.spotify.heroic.statistics.LocalMetadataManagerReporter;
@@ -74,156 +73,15 @@ public final class ElasticsearchSuggestModule implements SuggestModule {
         this.pools = Optional.fromNullable(pools).or(ReadWriteThreadPools.Config.provideDefault());
     }
 
-    private Map<String, XContentBuilder> mappings() throws IOException {
-        final Map<String, XContentBuilder> mappings = new HashMap<>();
-        mappings.put("tag", buildTagMapping());
-        mappings.put("series", buildSeriesMapping());
+    private Map<String, Map<String, Object>> mappings() throws IOException {
+        final Map<String, Map<String, Object>> mappings = new HashMap<>();
+        mappings.put("tag", JsonXContent.jsonXContent.createParser(inputStreamFromResource("tag.v1.json")).map());
+        mappings.put("series", JsonXContent.jsonXContent.createParser(inputStreamFromResource("series.v1.json")).map());
         return mappings;
     }
 
-    private XContentBuilder buildSeriesMapping() throws IOException {
-        final XContentBuilder b = XContentFactory.jsonBuilder();
-
-        // @formatter:off
-        b.startObject();
-          b.startObject(ElasticsearchUtils.TYPE_SERIES);
-            b.startObject("properties");
-              b.startObject(ElasticsearchUtils.KEY);
-                b.field("type", "string");
-                b.startObject("fields");
-                  b.startObject("raw");
-                    b.field("type", "string");
-                    b.field("index", "not_analyzed");
-                    b.field("doc_values", true);
-                  b.endObject();
-                b.endObject();
-              b.endObject();
-
-              b.startObject(ElasticsearchUtils.TAGS);
-                b.field("type", "nested");
-                b.startObject("properties");
-                  b.startObject(ElasticsearchUtils.TAGS_KEY);
-                    b.field("type", "string");
-                    b.startObject("fields");
-                      b.startObject("raw");
-                        b.field("type", "string");
-                        b.field("index", "not_analyzed");
-                        b.field("doc_values", true);
-                      b.endObject();
-                    b.endObject();
-                  b.endObject();
-
-                  b.startObject(ElasticsearchUtils.TAGS_VALUE);
-                    b.field("type", "string");
-                    b.startObject("fields");
-                      b.startObject("raw");
-                        b.field("type", "string");
-                        b.field("index", "not_analyzed");
-                        b.field("doc_values", true);
-                      b.endObject();
-                    b.endObject();
-                  b.endObject();
-                b.endObject();
-              b.endObject();
-              /* end tags */
-            b.endObject();
-          b.endObject();
-        b.endObject();
-        // @formatter:on
-
-        return b;
-    }
-
-    private XContentBuilder buildTagMapping() throws IOException {
-        final XContentBuilder b = XContentFactory.jsonBuilder();
-
-        // @formatter:off
-        b.startObject();
-          b.startObject(ElasticsearchUtils.TYPE_TAG);
-            b.startObject("properties");
-
-              b.startObject(ElasticsearchUtils.TAG_KEY);
-                b.field("type", "string");
-                b.startObject("fields");
-                  b.startObject("raw");
-                    b.field("type", "string");
-                    b.field("index", "not_analyzed");
-                    b.field("doc_values", true);
-                  b.endObject();
-                b.endObject();
-              b.endObject();
-
-              b.startObject(ElasticsearchUtils.TAG_VALUE);
-                b.field("type", "string");
-                b.startObject("fields");
-                  b.startObject("raw");
-                    b.field("type", "string");
-                    b.field("index", "not_analyzed");
-                    b.field("doc_values", true);
-                  b.endObject();
-                b.endObject();
-              b.endObject();
-
-              b.startObject(ElasticsearchUtils.TAG_KV);
-                b.field("type", "string");
-                b.field("index", "not_analyzed");
-                b.field("doc_values", true);
-              b.endObject();
-
-              // series is solely used for filtering, nothing should be stored or analyzed.
-              b.startObject(ElasticsearchUtils.TAG_SERIES);
-                b.field("type", "nested");
-                b.field("include_in_parent", true);
-                b.startObject("properties");
-                  // used for cardinality aggregation.
-                  b.startObject(ElasticsearchUtils.ID);
-                    b.field("type", "string");
-                    b.field("index", "not_analyzed");
-                    b.field("doc_values", true);
-                  b.endObject();
-
-                  b.startObject(ElasticsearchUtils.KEY);
-                    b.field("type", "string");
-                    b.field("index", "not_analyzed");
-                    b.field("doc_values", true);
-                    b.field("store", false);
-                  b.endObject();
-
-                  b.startObject(ElasticsearchUtils.TAGS);
-                    b.field("type", "nested");
-                    b.startObject("properties");
-                      b.startObject(ElasticsearchUtils.TAGS_KEY);
-                        b.field("type", "string");
-                        b.startObject("fields");
-                          b.startObject("raw");
-                            b.field("type", "string");
-                            b.field("index", "not_analyzed");
-                            b.field("doc_values", true);
-                          b.endObject();
-                        b.endObject();
-                      b.endObject();
-
-                      b.startObject(ElasticsearchUtils.TAGS_VALUE);
-                        b.field("type", "string");
-                        b.startObject("fields");
-                          b.startObject("raw");
-                            b.field("type", "string");
-                            b.field("index", "not_analyzed");
-                            b.field("doc_values", true);
-                          b.endObject();
-                        b.endObject();
-                      b.endObject();
-                    b.endObject();
-                  b.endObject();
-                  /* end tags */
-                b.endObject();
-              b.endObject();
-            b.endObject();
-          b.endObject();
-        b.endObject();
-        // @formatter:on
-
-        return b;
+    private InputStream inputStreamFromResource(String string) {
+        return getClass().getClassLoader().getResourceAsStream(string);
     }
 
     @Override
@@ -244,7 +102,7 @@ public final class ElasticsearchSuggestModule implements SuggestModule {
             @Provides
             @Singleton
             public Managed<Connection> connection(ManagedConnectionFactory connection) throws IOException {
-                return connection.construct(TEMPLATE_NAME, mappings());
+                return connection.constructDefaultSettings(TEMPLATE_NAME, mappings());
             }
 
             @Override
