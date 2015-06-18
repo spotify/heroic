7990cfea689140ad26cf33dc15ceeedd40002449
Javier Uruen Val <juruen@spotify.com>
2015-06-18 16:59:50 +0200

    [debian] fix build scripts
    - Fix broken mvn.sh script
    - Add dist as an argument to prepare-sources to support different
      distribution modules

--- debian/mvn.sh
+++ debian/mvn.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 MVN=mvn
 # quiet
-MVN+=-q
+MVN="${MVN} -q"
 # release should not be running tests
-MVN+=-D maven.test.skip=true
+MVN="${MVN} -D maven.test.skip=true"
 # use scoped home directory to avoid contaminating build system
-MVN+=-D user.home=$(MAVEN_HOME)
+MVN="${MVN} -D user.home=${MAVEN_HOME}"
 
 if [[ -f debian/settings.xml ]]; then
     # use in-project settings.xml
-    MVN+=-s debian/settings.xml
+    MVN="${MVN} -s debian/settings.xml"
 fi
 
 exec $MVN "$@"
