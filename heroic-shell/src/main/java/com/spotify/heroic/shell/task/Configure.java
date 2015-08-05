e87e96c6b1dee3a7071892574614195671944318
John-John Tedro <udoprog@spotify.com>
2015-08-05 17:29:02 +0200

    [shell] introduce configure (configure)

---
+++ heroic-shell/src/main/java/com/spotify/heroic/shell/task/Configure.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2015 Spotify AB.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.spotify.heroic.shell.task;
+
+import java.io.PrintWriter;
+
+import lombok.ToString;
+
+import org.kohsuke.args4j.Option;
+
+import com.google.inject.Inject;
+import com.spotify.heroic.metric.MetricManager;
+import com.spotify.heroic.shell.AbstractShellTask;
+import com.spotify.heroic.shell.AbstractShellTaskParams;
+import com.spotify.heroic.shell.CoreBridge;
+import com.spotify.heroic.shell.ShellTaskParams;
+import com.spotify.heroic.shell.ShellTaskUsage;
+
+import eu.toolchain.async.AsyncFuture;
+
+@ShellTaskUsage("Configure the given group of metric backends")
+public class Configure extends AbstractShellTask {
+    public static void main(String argv[]) throws Exception {
+        CoreBridge.standalone(argv, Configure.class);
+    }
+
+    @Inject
+    private MetricManager metrics;
+
+    @Override
+    public ShellTaskParams params() {
+        return new Parameters();
+    }
+
+    @Override
+    public AsyncFuture<Void> run(final PrintWriter out, final ShellTaskParams base) throws Exception {
+        final Parameters params = (Parameters) base;
+        return metrics.useGroup(params.group).configure();
+    }
+
+    @ToString
+    private static class Parameters extends AbstractShellTaskParams {
+        @Option(name = "-g", aliases = { "--group" }, usage = "Backend group to use", metaVar = "<group>")
+        private String group = null;
+    }
+}
