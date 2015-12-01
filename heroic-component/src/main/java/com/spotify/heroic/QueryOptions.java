4b54da3a72013e34f433acfcbfa9221320dcac5e
John-John Tedro <udoprog@spotify.com>
2015-12-01 10:05:00 +0100

    [component] make fetch size configurable for QueryOptions

--- heroic-component/src/main/java/com/spotify/heroic/QueryOptions.java
+++ heroic-component/src/main/java/com/spotify/heroic/QueryOptions.java
@@ -23,8 +23,11 @@ package com.spotify.heroic;
 
 import static java.util.Optional.ofNullable;
 
+import java.util.Optional;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.spotify.heroic.metric.QueryTrace;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -33,7 +36,7 @@ import lombok.RequiredArgsConstructor;
 public class QueryOptions {
     public static final boolean DEFAULT_TRACING = false;
 
-    public static final QueryOptions DEFAULTS = new QueryOptions(DEFAULT_TRACING);
+    public static final QueryOptions DEFAULTS = new QueryOptions(DEFAULT_TRACING, Optional.empty());
 
     // XXX: remove ones deployed everywhere.
     @Getter
@@ -49,13 +52,24 @@ public class QueryOptions {
      */
     private final boolean tracing;
 
+    /**
+     * The number of entries to fetch for every batch.
+     */
+    private final Optional<Integer> fetchSize;
+
     @JsonCreator
-    public QueryOptions(@JsonProperty("tracing") Boolean tracing) {
+    public QueryOptions(@JsonProperty("tracing") Boolean tracing,
+            @JsonProperty("fetchSize") Optional<Integer> fetchSize) {
         this.tracing = ofNullable(tracing).orElse(DEFAULT_TRACING);
+        this.fetchSize = fetchSize;
     }
 
     public boolean isTracing() {
         return tracing;
+    }
+
+    public Optional<Integer> getFetchSize() {
+        return fetchSize;
     }
 
     public static QueryOptions defaults() {
@@ -68,14 +82,20 @@ public class QueryOptions {
 
     public static class Builder {
         private boolean tracing = false;
+        private Optional<Integer> fetchSize = Optional.empty();
 
         public Builder tracing(boolean tracing) {
             this.tracing = tracing;
             return this;
         }
 
+        public Builder fetchSize(int fetchSize) {
+            this.fetchSize = Optional.of(fetchSize);
+            return this;
+        }
+
         public QueryOptions build() {
-            return new QueryOptions(tracing);
+            return new QueryOptions(tracing, fetchSize);
         }
     }
 }
