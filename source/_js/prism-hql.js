72fb6d32d13494a1fc4f2cd9c4f50c90ef06c49d
John-John Tedro <udoprog@tedro.se>
2016-04-13 22:08:19 +0200

    [docs] more documentation on upcoming complex queries

--- source/_js/prism-hql.js
+++ source/_js/prism-hql.js
@@ -3,6 +3,6 @@ Prism.languages.hql = {
 };
 
 Prism.languages.insertBefore('hql', 'string', {
-  'operator': /(\+|-|!=|=|!\^|\^|!)|(\b(and|or|not|in)\b)/g,
-  'keyword': /(\$key|\$now|<[a-z]+>|\b[0-9]+(ms|s|m|H|d|w)\b)/g
+  'operator': /(\+|-|!=|=|!\^|\^|!)|(\b(and|or|not|in|with|as|by)\b)/g,
+  'keyword': /(\$[a-z]+|\$now|<[a-z]+>|\b[0-9]+(ms|s|m|H|d|w)\b)/g
 });
