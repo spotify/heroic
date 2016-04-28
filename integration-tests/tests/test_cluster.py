00f8ad7a76362240fc39f3172064469af74a898b
John-John Tedro <udoprog@spotify.com>
2016-04-28 13:35:15 +0200

    [integration-tests] fix integration tests

--- integration-tests/tests/test_cluster.py
+++ integration-tests/tests/test_cluster.py
@@ -20,7 +20,7 @@ class TestCluster(unittest.TestCase):
             self.assertEquals(1, len(result['result']))
 
             self.assertEquals(
-                [[10, 4.0], [20, 4.0], [30, 2.0]],
+                [[0, 4.0], [10, 4.0], [20, 2.0]],
                 result['result'][0]['values'])
 
     def test_sharded_query(self):
@@ -30,7 +30,7 @@ class TestCluster(unittest.TestCase):
             self.assertEquals(2, len(result['result']))
 
             self.assertEquals(
-                [[[10, 1.0], [30, 2.0]], [[10, 3.0], [20, 4.0]]],
+                [[[0, 1.0], [20, 2.0]], [[0, 3.0], [10, 4.0]]],
                 [r['values'] for r in result['result']])
 
     @contextlib.contextmanager
