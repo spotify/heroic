dc787f45a5642c8ea63e25563596caf9222b7f50
John-John Tedro <udoprog@tedro.se>
2016-10-07 20:38:08 +0200

    [dist] test for cadence in ClusterQueryIT

--- heroic-dist/src/test/java/com/spotify/heroic/ClusterQueryIT.java
+++ heroic-dist/src/test/java/com/spotify/heroic/ClusterQueryIT.java
@@ -1,5 +1,6 @@
 package com.spotify.heroic;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.spotify.heroic.ingestion.Ingestion;
 import com.spotify.heroic.ingestion.IngestionManager;
@@ -10,6 +11,7 @@ import eu.toolchain.async.AsyncFuture;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -22,12 +24,10 @@ public class ClusterQueryIT extends AbstractLocalClusterIT {
     public void basicQueryTest() throws Exception {
         final QueryResult result = query("sum(10ms)");
 
-        final Set<MetricCollection> m = result
-            .getGroups()
-            .stream()
-            .map(ShardedResultGroup::getMetrics)
-            .collect(Collectors.toSet());
+        final Set<MetricCollection> m = getResults(result);
+        final List<Long> cadences = getCadences(result);
 
+        assertEquals(ImmutableList.of(10L), cadences);
         assertEquals(ImmutableSet.of(points().p(10, 2D).p(20, 4D).p(30, 2D).build()), m);
     }
 
@@ -35,12 +35,10 @@ public class ClusterQueryIT extends AbstractLocalClusterIT {
     public void distributedQueryTest() throws Exception {
         final QueryResult result = query("sum(10ms) by shared");
 
-        final Set<MetricCollection> m = result
-            .getGroups()
-            .stream()
-            .map(ShardedResultGroup::getMetrics)
-            .collect(Collectors.toSet());
+        final Set<MetricCollection> m = getResults(result);
+        final List<Long> cadences = getCadences(result);
 
+        assertEquals(ImmutableList.of(10L), cadences);
         assertEquals(ImmutableSet.of(points().p(10, 2D).p(20, 4D).p(30, 2D).build()), m);
     }
 
@@ -48,12 +46,10 @@ public class ClusterQueryIT extends AbstractLocalClusterIT {
     public void distributedDifferentQueryTest() throws Exception {
         final QueryResult result = query("sum(10ms) by diff");
 
-        final Set<MetricCollection> m = result
-            .getGroups()
-            .stream()
-            .map(ShardedResultGroup::getMetrics)
-            .collect(Collectors.toSet());
+        final Set<MetricCollection> m = getResults(result);
+        final List<Long> cadences = getCadences(result);
 
+        assertEquals(ImmutableList.of(10L, 10L), cadences);
         assertEquals(ImmutableSet.of(points().p(10, 1D).p(30, 2D).build(),
             points().p(10, 1D).p(20, 4D).build()), m);
     }
@@ -62,12 +58,21 @@ public class ClusterQueryIT extends AbstractLocalClusterIT {
     public void filterQueryTest() throws Exception {
         final QueryResult result = query("average(10ms) by * | topk(2) | bottomk(1) | sum(10ms)");
 
-        final Set<MetricCollection> m = result
-            .getGroups()
-            .stream()
-            .map(ShardedResultGroup::getMetrics)
-            .collect(Collectors.toSet());
+        final Set<MetricCollection> m = getResults(result);
+        final List<Long> cadences = getCadences(result);
 
+        assertEquals(ImmutableList.of(10L), cadences);
+        assertEquals(ImmutableSet.of(points().p(10, 1D).p(20, 4D).build()), m);
+    }
+
+    @Test
+    public void filterLastQueryTest() throws Exception {
+        final QueryResult result = query("average(10ms) by * | topk(2) | bottomk(1)");
+
+        final Set<MetricCollection> m = getResults(result);
+        final List<Long> cadences = getCadences(result);
+
+        assertEquals(ImmutableList.of(10L), cadences);
         assertEquals(ImmutableSet.of(points().p(10, 1D).p(20, 4D).build()), m);
     }
 
@@ -75,12 +80,10 @@ public class ClusterQueryIT extends AbstractLocalClusterIT {
     public void cardinalityTest() throws Exception {
         final QueryResult result = query("cardinality(10ms)");
 
-        final Set<MetricCollection> m = result
-            .getGroups()
-            .stream()
-            .map(ShardedResultGroup::getMetrics)
-            .collect(Collectors.toSet());
+        final Set<MetricCollection> m = getResults(result);
+        final List<Long> cadences = getCadences(result);
 
+        assertEquals(ImmutableList.of(10L), cadences);
         assertEquals(ImmutableSet.of(points().p(10, 1D).p(20, 1D).p(30, 1D).p(40, 0D).build()), m);
     }
 
@@ -89,12 +92,10 @@ public class ClusterQueryIT extends AbstractLocalClusterIT {
         // TODO: support native booleans in expressions
         final QueryResult result = query("cardinality(10ms, method=hllp(includeKey=\"true\"))");
 
-        final Set<MetricCollection> m = result
-            .getGroups()
-            .stream()
-            .map(ShardedResultGroup::getMetrics)
-            .collect(Collectors.toSet());
+        final Set<MetricCollection> m = getResults(result);
+        final List<Long> cadences = getCadences(result);
 
+        assertEquals(ImmutableList.of(10L), cadences);
         assertEquals(ImmutableSet.of(points().p(10, 2D).p(20, 1D).p(30, 1D).p(40, 0D).build()), m);
     }
 
@@ -115,5 +116,24 @@ public class ClusterQueryIT extends AbstractLocalClusterIT {
             .write(new Ingestion.Request(s2, points().p(10, 1D).p(20, 4D).build())));
 
         return writes;
+    }
+
+    private Set<MetricCollection> getResults(final QueryResult result) {
+        return result
+            .getGroups()
+            .stream()
+            .map(ShardedResultGroup::getMetrics)
+            .collect(Collectors.toSet());
+    }
+
+    private List<Long> getCadences(final QueryResult result) {
+        final List<Long> cadences = result
+            .getGroups()
+            .stream()
+            .map(ShardedResultGroup::getCadence)
+            .collect(Collectors.toList());
+
+        Collections.sort(cadences);
+        return cadences;
     }
 }
