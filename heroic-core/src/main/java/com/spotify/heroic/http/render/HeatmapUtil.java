6983b20b5065af103b400c6482b8d7a0bbc326f7
lucile <lucile.coutouly@obs-nancay.fr>
2017-05-24 12:43:15 +0200

    direct heatmap

--- heroic-core/src/main/java/com/spotify/heroic/http/render/HeatmapUtil.java
+++ heroic-core/src/main/java/com/spotify/heroic/http/render/HeatmapUtil.java
@@ -24,22 +24,10 @@ package com.spotify.heroic.http.render;
 import com.spotify.heroic.metric.MetricCollection;
 import com.spotify.heroic.metric.MetricType;
 import com.spotify.heroic.metric.Point;
+import com.spotify.heroic.metric.SeriesValues;
 import com.spotify.heroic.metric.ShardedResultGroup;
 import com.spotify.heroic.metric.Spread;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.plot.ValueMarker;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.DeviationRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.data.xy.YIntervalSeries;
-import org.jfree.data.xy.YIntervalSeriesCollection;
+
 
 import java.awt.BasicStroke;
 import java.awt.Color;
@@ -50,10 +38,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.Vector;
 
 public final class HeatmapUtil {
     private static final List<Color> COLORS = new ArrayList<>();
-
+    public int height;
+    public Vector v;
     static {
         COLORS.add(Color.BLUE);
     }
@@ -62,63 +53,75 @@ public final class HeatmapUtil {
         final List<ShardedResultGroup> groups, final String title, Map<String, String> highlight,
         Double threshold, int height
     ) {
-        final XYLineAndShapeRenderer lineAndShapeRenderer = new XYLineAndShapeRenderer(true, true);
-        final DeviationRenderer intervalRenderer = new DeviationRenderer();
 
-        final XYSeriesCollection regularData = new XYSeriesCollection();
-        final YIntervalSeriesCollection intervalData = new YIntervalSeriesCollection();
-
-        int lineAndShapeCount = 0;
-        int intervalCount = 0;
 
         for (final ShardedResultGroup resultGroup : groups) {
-            final MetricCollection group = resultGroup.getMetrics();
 
+            final MetricCollection group = (MetricCollection) resultGroup.getMetrics();
+            final SeriesValues series =(SeriesValues) SeriesValues.fromSeries(resultGroup.getSeries().iterator());
+            //System.out.print(group);
+            height = group.size();
             if (group.getType() == MetricType.POINT) {
-                final XYSeries series = new XYSeries(resultGroup.getMetrics().toString());
+                //System.out.print(group.getMetrics().toString());
 
                 final List<Point> data = group.getDataAs(Point.class);
+                //System.out.print(group.size());
 
+                Map<String, SortedSet<String>> tags =(Map<String, SortedSet<String>>) series.getTags();
+                //double v = pair.getValue();
+                //System.out.print(v);
+
+                /**
+                for (final Map.Entry<String, SortedSet<String>> pair : tags.entrySet()) {
+
+
+
+                    final SortedSet<String> values = pair.getValue();
+                    System.out.print("values :");
+                    System.out.println(values);
+                    System.out.print("Key : ");
+                    System.out.println(pair.getKey());
+                    if (values.size() != 1) {
+                        continue;
+                    }
+
+
+                }
+                 */
                 for (final Point d : data) {
-                    series.add(d.getTimestamp(), d.getValue());
+                    double t = (double) d.getTimestamp();
+
+                    double v = (double) d.getValue();
+                    System.out.print("timestamp : ");
+                    System.out.println(t);
+                    System.out.print("value : ");
+                    System.out.println(v);
+                    //String K = pair.getKey();
+                    //System.out.print(K);
+
                 }
-
-                //lineAndShapeRenderer.setSeriesPaint(lineAndShapeCount, Color.BLUE);
-                //lineAndShapeRenderer.setSeriesShapesVisible(lineAndShapeCount, false);
-                //lineAndShapeRenderer.setSeriesStroke(lineAndShapeCount, new BasicStroke(2.0f));
-                //regularData.addSeries(series);
-                //++lineAndShapeCount;
+                SortedSet<String> f = tags.get("f");
+                System.out.print("f : ");
+                System.out.print(f);
+                SortedSet<String> coor = tags.get("coor");
+                System.out.print("coor : ");
+                System.out.print(coor);
             }
+            System.out.print(group.size());
 
-            if (group.getType() == MetricType.SPREAD) {
-                final YIntervalSeries series =
-                    new YIntervalSeries(resultGroup.getMetrics().toString());
 
-                final List<Spread> data = group.getDataAs(Spread.class);
-
-                for (final Spread d : data) {
-                    series.add(d.getTimestamp(), d.getSum() / d.getCount(), d.getMin(), d.getMax());
-                }
-
-                //intervalRenderer.setSeriesPaint(intervalCount, Color.GREEN);
-                //intervalRenderer.setSeriesStroke(intervalCount, new BasicStroke(2.0f));
-                //intervalRenderer.setSeriesFillPaint(intervalCount, new Color(200, 255, 200));
-                //intervalRenderer.setSeriesShapesVisible(intervalCount, false);
-                //intervalData.addSeries(series);
-                //++intervalCount;
-            }
         }
 
 
         // final DateAxis rangeAxis = (DateAxis) plot.getRangeAxis();
         // rangeAxis.setStandardTickUnits(DateAxis.createStandardDateTickUnits());
-        double[][] data = new double[][]{{3,2,3,4,5,6},
+        double[][] datas = new double[][]{{3,2,3,4,5,6},
                                  {2,3,4,5,6,7},
                                  {3,4,5,6,7,6},
                                  {4,5,6,7,6,5}};
 
         // Step 1: Create our heat map chart using our data.
-        HeatChart map = new HeatChart(data);
+        HeatChart map = new HeatChart(datas);
 
         // Step 2: Customise the chart.
         map.setTitle("This is my heat chart title");
