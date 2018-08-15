0cd78ed9351fa7b2581cef3154668b654b14d788
Jorge Cabrera <jocabrer@ciena.com>
2018-08-15 15:54:10 -0700

    Better Cassandra pooling options config

--- metric/datastax/src/main/java/com/spotify/heroic/metric/datastax/DatastaxPoolingOptions.java
+++ metric/datastax/src/main/java/com/spotify/heroic/metric/datastax/DatastaxPoolingOptions.java
@@ -1,6 +1,7 @@
 package com.spotify.heroic.metric.datastax;
 
 import java.util.Optional;
+import java.util.Map;
 import com.datastax.driver.core.Cluster.Builder;
 import com.datastax.driver.core.HostDistance;
 import com.datastax.driver.core.PoolingOptions;
@@ -12,40 +13,34 @@ import static java.util.Optional.empty;
 
 public class DatastaxPoolingOptions {
 
-    private final Optional<Integer> maxRequestsPerConnectionLocal;
-    private final Optional<Integer> maxRequestsPerConnectionRemote;
-    private final Optional<Integer> coreConnectionsPerHostLocal;
-    private final Optional<Integer> coreConnectionsPerHostRemote;
-    private final Optional<Integer> maxConnectionsPerHostLocal;
-    private final Optional<Integer> maxConnectionsPerHostRemote;
+    private final Optional<Map<HostDistance, Integer>> maxRequestsPerConnection;
+    private final Optional<Map<HostDistance, Integer>> coreConnectionsPerHost;
+    private final Optional<Map<HostDistance, Integer>> maxConnectionsPerHost;
+    private final Optional<Map<HostDistance, Integer>> newConnectionThreshold;
     private final Optional<Integer> maxQueueSize;
     private final Optional<Integer> poolTimeoutMillis;
     private final Optional<Integer> idleTimeoutSeconds;
     private final Optional<Integer> heartbeatIntervalSeconds;
 
     public DatastaxPoolingOptions() {
-        this(empty(), empty(), empty(), empty(), empty(), empty(), empty(), empty(), empty(), empty());
+        this(empty(), empty(), empty(), empty(), empty(), empty(), empty(), empty());
     }
 
     @JsonCreator
     public DatastaxPoolingOptions(
-        @JsonProperty("maxRequestsPerConnection.LOCAL") Optional<Integer> maxRequestsPerConnectionLocal,
-        @JsonProperty("maxRequestsPerConnection.REMOTE") Optional<Integer> maxRequestsPerConnectionRemote,
-        @JsonProperty("coreConnectionsPerHost.LOCAL") Optional<Integer> coreConnectionsPerHostLocal,
-        @JsonProperty("coreConnectionsPerHost.REMOTE") Optional<Integer> coreConnectionsPerHostRemote,
-        @JsonProperty("maxConnectionsPerHost.LOCAL") Optional<Integer> maxConnectionsPerHostLocal,
-        @JsonProperty("maxConnectionsPerHost.REMOTE") Optional<Integer> maxConnectionsPerHostRemote,
+        @JsonProperty("maxRequestsPerConnection") Optional<Map<HostDistance, Integer>> maxRequestsPerConnection,
+        @JsonProperty("coreConnectionsPerHost") Optional<Map<HostDistance, Integer>> coreConnectionsPerHost,
+        @JsonProperty("maxConnectionsPerHost") Optional<Map<HostDistance, Integer>> maxConnectionsPerHost,
+        @JsonProperty("newConnectionThreshold") Optional<Map<HostDistance, Integer>> newConnectionThreshold,
         @JsonProperty("maxQueueSize") Optional<Integer> maxQueueSize,
         @JsonProperty("poolTimeoutMillis") Optional<Integer> poolTimeoutMillis,
         @JsonProperty("idleTimeoutSeconds") Optional<Integer> idleTimeoutSeconds,
         @JsonProperty("heartbeatIntervalSeconds") Optional<Integer> heartbeatIntervalSeconds
     ){
-        this.maxRequestsPerConnectionLocal = maxRequestsPerConnectionLocal;
-        this.maxRequestsPerConnectionRemote = maxRequestsPerConnectionRemote;
-        this.coreConnectionsPerHostLocal = coreConnectionsPerHostLocal;
-        this.coreConnectionsPerHostRemote = coreConnectionsPerHostRemote;
-        this.maxConnectionsPerHostLocal = maxConnectionsPerHostLocal;
-        this.maxConnectionsPerHostRemote = maxConnectionsPerHostRemote;
+        this.maxRequestsPerConnection = maxRequestsPerConnection;
+        this.coreConnectionsPerHost = coreConnectionsPerHost;
+        this.maxConnectionsPerHost = maxConnectionsPerHost;
+        this.newConnectionThreshold = newConnectionThreshold;
         this.maxQueueSize = maxQueueSize;
         this.poolTimeoutMillis = poolTimeoutMillis;
         this.idleTimeoutSeconds = idleTimeoutSeconds;
@@ -54,12 +49,27 @@ public class DatastaxPoolingOptions {
 
     public void apply(final Builder builder) {
         final PoolingOptions pooling = new PoolingOptions();
-        this.maxRequestsPerConnectionLocal.ifPresent(x -> pooling.setMaxConnectionsPerHost(HostDistance.LOCAL, x));
-        this.maxRequestsPerConnectionRemote.ifPresent(x -> pooling.setMaxConnectionsPerHost(HostDistance.REMOTE, x));
-        this.coreConnectionsPerHostLocal.ifPresent(x -> pooling.setCoreConnectionsPerHost(HostDistance.LOCAL, x));
-        this.coreConnectionsPerHostRemote.ifPresent(x -> pooling.setCoreConnectionsPerHost(HostDistance.REMOTE, x));
-        this.maxConnectionsPerHostLocal.ifPresent(x -> pooling.setMaxConnectionsPerHost(HostDistance.LOCAL, x));
-        this.maxConnectionsPerHostRemote.ifPresent(x -> pooling.setMaxConnectionsPerHost(HostDistance.REMOTE, x));
+        this.maxRequestsPerConnection.ifPresent( x -> {
+            for (Map.Entry<HostDistance, Integer> entry : x.entrySet())
+                pooling.setMaxRequestsPerConnection(entry.getKey(), entry.getValue());
+            }
+        );
+        this.coreConnectionsPerHost.ifPresent( x -> {
+            for (Map.Entry<HostDistance, Integer> entry : x.entrySet())
+                pooling.setCoreConnectionsPerHost(entry.getKey(), entry.getValue());
+            }
+        );
+        this.maxConnectionsPerHost.ifPresent( x -> {
+            for (Map.Entry<HostDistance, Integer> entry : x.entrySet())
+                pooling.setMaxConnectionsPerHost(entry.getKey(), entry.getValue());
+            }
+        );
+        this.newConnectionThreshold.ifPresent( x -> {
+                for (Map.Entry<HostDistance, Integer> entry : x.entrySet())
+                    pooling.setNewConnectionThreshold(entry.getKey(), entry.getValue());
+            }
+        );
+
         this.maxQueueSize.ifPresent(pooling::setMaxQueueSize);
         this.poolTimeoutMillis.ifPresent(pooling::setPoolTimeoutMillis);
         this.idleTimeoutSeconds.ifPresent(pooling::setIdleTimeoutSeconds);
