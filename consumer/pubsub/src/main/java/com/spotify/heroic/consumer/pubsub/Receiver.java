044a401dbc075b2ade66620e2fd2ce3c0d1c7d9f
William Huba <whuba@spotify.com>
2018-09-06 10:40:41 -0400

    Add consumer metric reporting to PubSub.

--- consumer/pubsub/src/main/java/com/spotify/heroic/consumer/pubsub/Receiver.java
+++ consumer/pubsub/src/main/java/com/spotify/heroic/consumer/pubsub/Receiver.java
@@ -26,15 +26,29 @@ import com.google.cloud.pubsub.v1.MessageReceiver;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.spotify.heroic.consumer.ConsumerSchema;
-import com.spotify.heroic.consumer.ConsumerSchemaException;
+import com.spotify.heroic.consumer.ConsumerSchemaValidationException;
+import com.spotify.heroic.statistics.ConsumerReporter;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class Receiver implements MessageReceiver {
     private final ConsumerSchema.Consumer consumer;
+    private final ConsumerReporter reporter;
+    private final AtomicLong errors;
+    private final LongAdder consumed;
 
-    Receiver(ConsumerSchema.Consumer consumer) {
+    Receiver(
+        final ConsumerSchema.Consumer consumer,
+        final ConsumerReporter reporter,
+        final AtomicLong errors,
+        final LongAdder consumed
+    ) {
         this.consumer = consumer;
+        this.reporter = reporter;
+        this.errors = errors;
+        this.consumed = consumed;
     }
 
     @Override
@@ -43,14 +57,25 @@ class Receiver implements MessageReceiver {
         final ByteString data = message.getData();
         final String messageId = message.getMessageId();
         log.debug("Received ID:{} with content: {}", messageId, data.toStringUtf8());
+        final byte[] bytes = data.toByteArray();
 
         // process the data
         try {
-            consumer.consume(data.toByteArray());
-        } catch (ConsumerSchemaException e) {
-            log.error("ID:{} - {}", messageId, e.getMessage(), e);
-        } finally {
+            consumer.consume(bytes);
+            reporter.reportMessageSize(bytes.length);
+            consumed.increment();
             replyConsumer.ack();
+        } catch (ConsumerSchemaValidationException e) {
+            reporter.reportConsumerSchemaError();
+            log.error("ID:{} - {}", messageId, e.getMessage(), e);
+
+            // The message will never be processable, ack it to make it go away
+            replyConsumer.ack();
+        } catch (Exception e) {
+            errors.incrementAndGet();
+            log.error("ID:{} - Failed to consume", messageId, e);
+            reporter.reportMessageError();
+            replyConsumer.nack();
         }
   }
 
