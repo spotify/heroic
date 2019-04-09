ebb79812ef15b25aea937c58224b4854e51be59f
William Huba <whuba@spotify.com>
2019-04-09 14:54:12 -0400

    Fix prism loading.

--- docs/assets/js/prism-hql.js
+++ docs/assets/js/prism-hql.js
@@ -1,8 +1,13 @@
-Prism.languages.hql = {
-  'string': /("(?!:)(\\?[^'"])*?"(?!:)|\b[a-z\.]+\b)/g
-};
+/* global Prism */
 
-Prism.languages.insertBefore('hql', 'string', {
-  'operator': /(\+|-|!=|=|!\^|\^|!)|(\b(and|or|not|in|with|as|by)\b)/g,
-  'keyword': /(\$[a-z]+|\$now|<[a-z]+>|\b[0-9]+(ms|s|m|H|d|w)\b)/g
-});
+(function () {
+
+  Prism.languages.hql = {
+    'string': /("(?!:)(\\?[^'"])*?"(?!:)|\b[a-z\.]+\b)/g
+  };
+
+  Prism.languages.insertBefore('hql', 'string', {
+    'operator': /(\+|-|!=|=|!\^|\^|!)|(\b(and|or|not|in|with|as|by)\b)/g,
+    'keyword': /(\$[a-z]+|\$now|<[a-z]+>|\b[0-9]+(ms|s|m|H|d|w)\b)/g
+  });
+}());
