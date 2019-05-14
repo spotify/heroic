0fb14d376f73fc5ff73baf739d77f25bbf5bdf8a
William Huba <whuba@spotify.com>
2019-05-14 15:32:35 -0400

    renaming some things

--- docs/assets/fta/pubsub_consumers.R
+++ docs/assets/fta/pubsub_consumers.R
@@ -12,21 +12,23 @@ elasticsearch_data_nodes <- 40
 metrics_per_second <- 1000000
 ## number of metrics each consumer can process per second
 consumer_capacity <- 30000
-## number of metadata consumers. if using autoscaling, set this to the autoscaler max
-metadata_consumers <- 35
+## number of consumers. if using autoscaling, set this to the autoscaler max
+consumers <- 35
 ################################################################################
+
+capacity_max <- ceiling(consumers - (metrics_per_second / consumer_capacity))
 
 tree <- ftree.make(type="or", name="PubSub consumer", name2="pipeline failure")
 
 tree <- addLogic(tree, at=1, type="or", tag="MetadataCluster", name="Metadata consumers", name2="cluster failure")
-tree <- addLogic(tree, at=1, type="or", tag="BigTableCluster", name="Metrics BT Consumer", name2="cluster failure")
+tree <- addLogic(tree, at=1, type="or", tag="BigtableCluster", name="Bigtable consumer", name2="cluster failure")
+
 ## PubSub failure
 ## https://cloud.google.com/pubsub/sla
 tree <- addProbability(tree, at="MetadataCluster", tag="PubSub", name="PubSub failure", prob=0.0005)
 
 ## Host failure events
-capacity_max <- ceiling(metadata_consumers - (metrics_per_second / consumer_capacity))
-tree <- addLogic(tree, at="MetadataCluster", type="vote", vote_par=c(capacity_max, metadata_consumers),
+tree <- addLogic(tree, at="MetadataCluster", type="vote", vote_par=c(capacity_max, consumers),
                  tag="Capacity", name="Consumer", name2="capacity exceeded")
 tree <- addLogic(tree, at="Capacity", type="or", tag="Host", name="Host failure")
 tree <- addProbability(tree, at="Host", tag="GCEInstance", name="GCE instance failure", prob=0.0001)
@@ -52,13 +54,13 @@ tree <- addLogic(tree, at="ElasticsearchShard", type="or", tag="ElasticsearchDat
                  name="Elasticsearch data", name2="node failure")
 tree <- addDuplicate(tree, at="ElasticsearchData", dup_id="Disk", collapse=TRUE)
 tree <- addDuplicate(tree, at="ElasticsearchData", dup_id="Host", collapse=TRUE)
-## BT Consumers
-tree <- addProbability(tree, at="BigTableCluster", tag="ZonalBT", name="Zonal failure", prob=0.001)
-tree <- addLogic(tree, at="BigTableCluster", type="vote", vote_par=c(capacity_max, metadata_consumers),
-                 tag="BTCapacity", name="Consumer", name2="capacity exceeded")
-tree <- addLogic(tree, at="BTCapacity", type="or", tag="BTCapacityHosts",
-                 name="BTCapacity", name2="Host failure")
-tree <- addDuplicate(tree, at="BTCapacityHosts", dup_id="Host", collapse=TRUE)
+
+## Bigtable consumers
+## https://cloud.google.com/bigtable/sla
+tree <- addProbability(tree, at="BigtableCluster", tag="ZonalBT", name="Zonal cluster failure", prob=0.001)
+tree <- addDuplicate(tree, at="BigtableCluster", dup_id="Capacity", collapse=TRUE)
+
+
 ## TODO: this displays all probabilities in scientic notation. It would be nice to make them into percentages.
 tree <- ftree.calc(tree)
 for (p in tree$PBF) {
