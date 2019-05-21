ba88933ddf832570d00343ab6a4ea0bdae363849
Drew Michel <dmichel@spotify.com>
2019-05-21 09:32:10 -0400

    [component] make unit nullable in SamplingQuery
    Before this was migrated to Kotlin unit was an optional/nullable field.

--- docs/content/_api_types/sampling-query.md
+++ docs/content/_api_types/sampling-query.md
@@ -3,14 +3,14 @@ type_name: SamplingQuery
 fields:
   - name: unit
     type_json: '&lt;string&gt;'
-    purpose: The default unit to use in size and extent (unless specified).
+    purpose: The default unit to use in size and extent (unless specified as shown below). E.g. `seconds`, `minutes`, `hours`.
   - name: size
     required: true
-    type_json: '&lt;number&gt; | {"unit": &lt;string&gt;, "value": &lt;number&gt;}'
-    purpose: The size to use in a given aggregation.
+    type_json: '&lt;number&gt; | 300s' 
+    purpose: The size to use in a given aggregation. Time unit defaults to seconds if not appended to the end or specified in unit. 
   - name: extent
     required: true
-    type_json: '&lt;number&gt; | {"unit": &lt;string&gt;, "value": &lt;number&gt;}'
+    type_json: '&lt;number&gt; | 300s'
     purpose: The extent to use in a given aggregation.
 ---
 Java Class: {% include github-link.html module='heroic-component' name='aggregation.SamplingQuery' %}
--- heroic-core/src/test/java/com/spotify/heroic/aggregation/SamplingQueryDeserializationTest.java
+++ heroic-core/src/test/java/com/spotify/heroic/aggregation/SamplingQueryDeserializationTest.java
@@ -1,18 +1,16 @@
 package com.spotify.heroic.aggregation;
 
+import static com.spotify.heroic.test.Resources.openResource;
+import static org.junit.Assert.assertEquals;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spotify.heroic.HeroicMappers;
 import com.spotify.heroic.common.Duration;
 import com.spotify.heroic.grammar.QueryParser;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.mockito.Mockito;
-
-import java.io.InputStream;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-
-import static com.spotify.heroic.test.Resources.openResource;
-import static org.junit.Assert.assertEquals;
 
 public class SamplingQueryDeserializationTest {
     private final ObjectMapper m = HeroicMappers.json(Mockito.mock(QueryParser.class));
@@ -30,5 +28,16 @@ public class SamplingQueryDeserializationTest {
             assertEquals(new SamplingQuery(null, d),
                 m.readValue(in, SamplingQuery.class));
         }
+
+        try (final InputStream in = openResource(getClass(), "SamplingQuery.3.json")) {
+            assertEquals(new SamplingQuery(d, d),
+              m.readValue(in, SamplingQuery.class));
+        }
+
+        try (final InputStream in = openResource(getClass(), "SamplingQuery.4.json")) {
+            assertEquals(new SamplingQuery(d, d),
+              m.readValue(in, SamplingQuery.class));
+        }
+
     }
 }
