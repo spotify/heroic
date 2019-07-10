25fda501bff6dbb4e3c68baa818f417cdb2ad8b5
William Huba <whuba@spotify.com>
2019-07-10 17:23:00 -0400

    [datastax] Remove legacy schema from datastax metric module.

--- metric/datastax/src/main/java/com/spotify/heroic/metric/datastax/schema/Schema.java
+++ metric/datastax/src/main/java/com/spotify/heroic/metric/datastax/schema/Schema.java
@@ -27,24 +27,22 @@ import com.datastax.driver.core.Session;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.spotify.heroic.metric.Point;
-import com.spotify.heroic.metric.datastax.schema.legacy.LegacySchema;
 import com.spotify.heroic.metric.datastax.schema.ng.NextGenSchema;
 import eu.toolchain.async.AsyncFuture;
 import eu.toolchain.async.Transform;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
-    @JsonSubTypes.Type(value = LegacySchema.class, name = "legacy"), @JsonSubTypes.Type(
-    value = NextGenSchema.class, name = "ng")
+    @JsonSubTypes.Type(value = NextGenSchema.class, name = "ng")
 })
 public interface Schema {
-    public AsyncFuture<Void> configure(final Session session);
+    AsyncFuture<Void> configure(final Session session);
 
-    public AsyncFuture<SchemaInstance> instance(final Session session);
+    AsyncFuture<SchemaInstance> instance(final Session session);
 
-    public static interface PreparedFetch {
-        public BoundStatement fetch(int limit);
+    interface PreparedFetch {
+        BoundStatement fetch(int limit);
 
-        public Transform<Row, Point> converter();
+        Transform<Row, Point> converter();
     }
 }
