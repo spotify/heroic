13979b23ff7821527d0adadb5cb0141aa4b298ae
Drew Michel <dmichel@spotify.com>
2019-07-17 14:46:34 -0400

    Use new dockerhub build process
    Instead of having travis trigger a dockerhub build, github has been configured to send a webhook to dockerhub to  build the image.
    Dockerhub will build the master branch and any tags and label the docker image appropriately.

--- .scripts/trigger_docker_hub.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Tag\", \"source_name\": \"$TRAVIS_TAG\"}" -X POST https://registry.hub.docker.com/u/spotify/heroic/trigger/$DOCKER_TOKEN/
