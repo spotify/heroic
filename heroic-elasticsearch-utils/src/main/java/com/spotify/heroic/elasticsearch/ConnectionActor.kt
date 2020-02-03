0191fd2bf5b33f09144e8d2f45ddaf9c2dec6562
William Huba <whuba@spotify.com>
2020-02-03 14:52:39 -0500

    ConnectionActor as typed actor

--- heroic-elasticsearch-utils/src/main/java/com/spotify/heroic/elasticsearch/ConnectionActor.kt
+++ heroic-elasticsearch-utils/src/main/java/com/spotify/heroic/elasticsearch/ConnectionActor.kt
@@ -21,57 +21,77 @@
 
 package com.spotify.heroic.elasticsearch
 
-import akka.actor.AbstractActor
-import akka.event.Logging
-import akka.event.LoggingAdapter
+import akka.actor.typed.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.javadsl.AbstractBehavior
+import akka.actor.typed.javadsl.ActorContext
+import akka.actor.typed.javadsl.Receive
 import com.spotify.heroic.elasticsearch.index.IndexMapping
 import org.elasticsearch.action.ActionListener
 import org.elasticsearch.action.DocWriteRequest
 import org.elasticsearch.action.ListenableActionFuture
 import org.elasticsearch.action.admin.indices.template.put.PutIndexTemplateResponse
+import org.elasticsearch.action.delete.DeleteRequestBuilder
 import org.elasticsearch.action.index.IndexResponse
+import org.elasticsearch.action.search.SearchRequestBuilder
+import org.elasticsearch.action.search.SearchScrollRequestBuilder
 import org.elasticsearch.common.xcontent.XContentBuilder
 
 class ConnectionActor(
     val index: IndexMapping,
     val client: ClientSetup.ClientWrapper,
     val templateName: String,
-    val type: BackendType
-): AbstractActor() {
-    private val log: LoggingAdapter = Logging.getLogger(context.system, this)
-
-    override fun createReceive(): Receive {
-        return receiveBuilder()
-            .matchEquals(Message.CLOSE, { client.shutdown.run() })
-            .matchEquals(Message.CONFIGURE, { configure() })
-            .matchEquals(Message.READ_INDICES, { sender.tell(index.readIndices(), self) })
-            .matchEquals(Message.WRITE_INDICES, { sender.tell(index.writeIndices(), self) })
-            .match(Message.Search::class.java) {
+    val type: BackendType,
+    context: ActorContext<Command>
+): AbstractBehavior<ConnectionActor.Command>(context) {
+    override fun createReceive(): Receive<Command> {
+        return newReceiveBuilder()
+            .onMessage(Command.Close::class.java, ::close)
+            .onMessage(Command.Configure::class.java, ::configure)
+            .onMessage(Command.ReadIndices::class.java) {
+                it.replyTo.tell(Response.Indices(index.readIndices()))
+                this
+            }
+            .onMessage(Command.WriteIndices::class.java) {
+                it.replyTo.tell(Response.Indices(index.writeIndices()))
+                this
+            }
+            .onMessage(Command.Search::class.java) {
                 val result = index.search(client.client, it.type)
-                sender.tell(result, self)
+                it.replyTo.tell(Response.Search(result))
+                this
             }
-            .match(Message.Count::class.java) {
+            .onMessage(Command.Count::class.java) {
                 val result = index.count(client.client, it.type)
-                sender.tell(result, self)
+                it.replyTo.tell(Response.Search(result))
+                this
             }
-            .match(Message.Index::class.java) {
-                sender.tell(index(it), self)
-            }
-            .match(Message.PrepareSearchScroll::class.java) {
+            .onMessage(Command.Index::class.java, ::index)
+            .onMessage(Command.PrepareSearchScroll::class.java) {
                 val result = client.client.prepareSearchScroll(it.scrollId)
-                sender.tell(result, self)
+                it.replyTo.tell(Response.SearchScroll(result))
+                this
             }
-            .match(Message.Delete::class.java) {
+            .onMessage(Command.Delete::class.java) {
                 val result = index.delete(client.client, it.type, it.id)
-                sender.tell(result, self)
+                it.replyTo.tell(Response.Delete(result))
+                this
             }
-            .matchAny { log.error("Received unknown message: {}", it) }
+            .onAnyMessage {
+                context.log.error("Received unknown message: {}", it)
+                this
+            }
             .build()
     }
 
-    fun configure(): PutIndexTemplateResponse {
+    fun close(command: Command.Close): Behavior<Command> {
+        client.shutdown.run()
+        return this
+    }
+
+    fun configure(command: Command.Configure): Behavior<Command> {
         val indices = client.client.admin().indices()
-        log.info("[{}] updating template for {}", templateName, index.template())
+        context.log.info("[{}] updating template for {}", templateName, index.template())
 
         val put = indices.preparePutTemplate(templateName)
             .setSettings(type.settings)
@@ -90,30 +110,47 @@ class ConnectionActor(
                 throw e
             }
         })
-        return target.get()
+
+        command.replyTo.tell(Response.Configure(target.get()))
+        return this
     }
 
-    fun index(request: Message.Index): ListenableActionFuture<IndexResponse> {
-        return client.client.prepareIndex(request.index, request.type)
+    fun index(request: Command.Index): Behavior<Command> {
+        val response = client.client.prepareIndex(request.index, request.type)
             .setId(request.id)
             .setSource(request.source)
             .setOpType(request.opType)
             .execute()
+        request.replyTo.tell(Response.Index(response))
+        return this
     }
 
-    enum class Message {
-        CLOSE, CONFIGURE, READ_INDICES, WRITE_INDICES;
+    interface Command {
+        data class Close(val replyTo: ActorRef<Response>): Command
+        data class Configure(val replyTo: ActorRef<Response>): Command
+        data class ReadIndices(val replyTo: ActorRef<Response>): Command
+        data class WriteIndices(val replyTo: ActorRef<Response>): Command
 
-        data class Search(val type: String)
-        data class Count(val type: String)
+        data class Search(val type: String, val replyTo: ActorRef<Response>): Command
+        data class Count(val type: String, val replyTo: ActorRef<Response>): Command
         data class Index(
             val index: String,
             val type: String,
             val id: String,
             val source: XContentBuilder,
-            val opType: DocWriteRequest.OpType
-        )
-        data class PrepareSearchScroll(val scrollId: String)
-        data class Delete(val type: String, val id: String)
+            val opType: DocWriteRequest.OpType,
+            val replyTo: ActorRef<Response>
+        ): Command
+        data class PrepareSearchScroll(val scrollId: String, val replyTo: ActorRef<Response>): Command
+        data class Delete(val type: String, val id: String, val replyTo: ActorRef<Response>): Command
+    }
+
+    interface Response {
+        data class Configure(val response: PutIndexTemplateResponse): Response
+        data class Indices(val indices: Array<String>): Response
+        data class Search(val builder: SearchRequestBuilder): Response
+        data class Index(val future: ListenableActionFuture<IndexResponse>): Response
+        data class SearchScroll(val builder: SearchScrollRequestBuilder): Response
+        data class Delete(val builders: List<DeleteRequestBuilder>): Response
     }
 }
